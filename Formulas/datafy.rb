d2a79ee129da70041e8697eb46100fbe6b3d4e3b
Stijn De Haes <stijndehaes@gmail.com>
2020-04-02 21:43:01 +0200

    added brew formula

---
+++ Formulas/datafy.rb
@@ -0,0 +1,11 @@
+class Heft < Formula
+  desc "Datafy command line interface"
+  homepage "https://get.datafy.cloud/"
+  url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.12.0/datafy_darwin_amd64.tar.gz"
+  version "0.12.0"
+  sha256 "d1647e75702c51f0aa8800ab299b52ecee19265fdf80e2d1e538540b990a885d"
+
+  def install
+    bin.install "godatafy"
+  end
+end
