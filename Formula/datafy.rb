0ab7275fb4aad3a8f6226793257aa8235169d3fd
Stijn De Haes <stijndehaes@gmail.com>
2020-07-24 16:21:39 +0200

    Release 0.21.0
    Upgrade datafy command from godatafy to datafy

--- Formula/datafy.rb
+++ Formula/datafy.rb
@@ -1,11 +1,11 @@
 class Datafy < Formula
   desc "Datafy command line interface"
   homepage "https://get.datafy.cloud/"
-  url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.20.1/datafy_darwin_amd64.tar.gz"
-  version "0.20.1"
-  sha256 "ba6543047c10379d22165c3e9b0f6ff601b1aea0e9a5311075c5efc031c049b0"
+  url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.21.0/datafy_darwin_amd64.tar.gz"
+  version "0.21.0"
+  sha256 "79c8e42343534a3c5c52f25866e477a96b834dec236790cfdfa2401fd1b13242"
 
   def install
-    bin.install "darwin/amd64/datafy" => "godatafy"
+    bin.install "darwin/amd64/datafy" => "datafy"
   end
 end
