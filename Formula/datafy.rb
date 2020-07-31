aea9c44a2d82d40de59118a36aaffd03735571a1
Stijn De Haes <stijndehaes@gmail.com>
2020-07-31 13:50:59 +0200

    Update to 0.22.0 and install zsh and bash completions

--- Formula/datafy.rb
+++ Formula/datafy.rb
@@ -1,11 +1,19 @@
 class Datafy < Formula
   desc "Datafy command line interface"
   homepage "https://get.datafy.cloud/"
-  url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.21.0/datafy_darwin_amd64.tar.gz"
-  version "0.21.0"
-  sha256 "79c8e42343534a3c5c52f25866e477a96b834dec236790cfdfa2401fd1b13242"
+  url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.22.0/datafy_darwin_amd64.tar.gz"
+  version "0.22.0"
+  sha256 "8db1980d1b67acebef238b4dee70c16258a0fa547c4869aa3ba26c25b58197f2"
 
   def install
     bin.install "darwin/amd64/datafy" => "datafy"
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/datafy", "completion", "bash", { :err => :out })
+    (bash_completion/"datafy").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/datafy", "completion", "zsh", { :err => :out })
+    (zsh_completion/"_datafy").write output
   end
 end
