35b907aa41640e3f75d97560747a023e5247b03e
Stijn De Haes <stijndehaes@gmail.com>
2020-11-12 12:56:44 +0000

    Update to 0.31.2

--- Formula/datafy.rb
+++ Formula/datafy.rb
@@ -1,9 +1,9 @@
 class Datafy < Formula
   desc "Datafy command line interface"
   homepage "https://get.datafy.cloud/"
-  url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.31.1/datafy_darwin_amd64.tar.gz"
-  version "0.31.1"
-  sha256 "fa6333eb06a5c714e9c7482637543631e69a99f659639d1700004eb49ef36ac7"
+  url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.31.2/datafy_darwin_amd64.tar.gz"
+  version "0.31.2"
+  sha256 "d42978850b3033903a331be3bacb847078b32bae94f826dc468738e0845e5307"
 
   def install
     bin.install "darwin/amd64/datafy" => "datafy"
