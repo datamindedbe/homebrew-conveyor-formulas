c0535d0046337890d1063405324d51bf17287f9b
Jonathan Merlevede <jonathan@merleve.de>
2021-04-06 22:51:37 +0200

    Added release script

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # Homebrew Datafy formulas
 
-
 Allow you to install the datafy cli using `brew install datamindedbe/datafy/datafy`.
+
+
+# Updating formula
+
+To upgrade the formula to point the latest Datafy release, simply run `release.py`. This will:
+* Detect the latest version available for download (from https://app.datafy.cloud/api/info/cli/version)
+* If the version is greater than the version in the formula, will update the formula
+* Will download the installation tar files in memory to compute SHA256 hashes
+* Commit the changes to master and push
+
+To test a formula, you can point `brew` directly to a formula source
+```
+brew install --build-from-source datafy.rb
+```
+
+To test the Linux build, you can run `brew` in docker
+```
+docker run -v `pwd`:/mnt/brew -it homebrew/brew bash
+```
---
+++ release.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import os, re, subprocess, hashlib
+from pathlib import Path
+import urllib.request as ur
+
+datafy_cmd="datafy"
+latest_version_url="https://app.datafy.cloud/api/info/cli/version"
+version_to_release_urls = lambda version: {
+    "macos": f"https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/{version}/datafy_darwin_amd64.tar.gz",
+    "linux": f"https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/{version}/datafy_linux_amd64.tar.gz",
+}
+formula_path = Path(os.path.realpath(__file__)).parent.joinpath("Formula", "datafy.rb")
+released_version=re.search("version\s+\"([\d\.]+)", formula_path.read_text())[1]
+latest_version=ur.urlopen(latest_version_url).read().decode('utf-8')
+
+print(f"Released version: {released_version}")
+print(f"Latest version: {latest_version}")
+if latest_version == released_version:
+    print("Good news! Latest version already released; exiting")
+    exit()
+
+release_urls = version_to_release_urls(latest_version)
+print(f"Downloading releases from {release_urls}")
+shas = {platform: hashlib.sha256(ur.urlopen(url).read()).hexdigest() for platform, url in release_urls.items()}
+print(f"Release hashes: {shas}")
+blocks = {}
+for key in release_urls:
+  blocks[key] = f"""on_{key} do
+    url "{release_urls[key]}"
+    sha256 "{shas[key]}"
+  end"""
+
+formula=f"""class Datafy < Formula
+  desc "Datafy command line interface"
+  homepage "https://get.datafy.cloud/"
+  {blocks['macos']}
+  {blocks['linux']}
+  version "{latest_version}"
+
+  def install
+    bin.install Dir.glob("**/datafy")
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{{bin}}/datafy", "completion", "bash", {{ :err => :out }})
+    (bash_completion/"datafy").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{{bin}}/datafy", "completion", "zsh", {{ :err => :out }})
+    (zsh_completion/"datafy").write output
+  end
+end"""
+formula_path.write_text(formula)
+subprocess.run(f"git add . && git commit -m 'upgrade to v{latest_version}' && git push")
