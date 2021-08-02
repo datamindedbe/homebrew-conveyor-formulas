21b1d6558400147e035b86781d1aa81ac658a414
Stijn De Haes <stijndehaes@gmail.com>
2021-08-02 09:14:08 +0200

    upgrade to v0.52.0

--- Formula/datafy.rb
+++ Formula/datafy.rb
@@ -2,14 +2,14 @@ class Datafy < Formula
   desc "Datafy command line interface"
   homepage "https://get.datafy.cloud/"
   on_macos do
-    url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.51.0/datafy_darwin_amd64.tar.gz"
-    sha256 "cf8b0b5dcdf765b6643472fd6f2bb58143e7a4c6fea3c38ee1d870ec5f943eb9"
+    url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.52.0/datafy_darwin_amd64.tar.gz"
+    sha256 "a40e1042d1ba3df188206d9812c5f7e687a963f4f8f80cc87d2c855d7d122c38"
   end
   on_linux do
-    url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.51.0/datafy_linux_amd64.tar.gz"
-    sha256 "edc8924f390abbfba69709ed5dfb9b492c57dc70e1dbf6bdc48aa3257ec440c6"
+    url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.52.0/datafy_linux_amd64.tar.gz"
+    sha256 "b368b5245793a95458e7e977e61a0cad4b8500159a59c468710f2ca18c8655f9"
   end
-  version "0.51.0"
+  version "0.52.0"
 
   def install
     bin.install Dir.glob("**/datafy")
@@ -20,10 +20,6 @@ class Datafy < Formula
 
     # Install zsh completion
     output = Utils.safe_popen_read("#{bin}/datafy", "completion", "zsh", { :err => :out })
-    (zsh_completion/"_datafy").write output
-
-    # Install fish completion
-    output = Utils.safe_popen_read("#{bin}/datafy", "completion", "fish", { :err => :out })
-    (fish_completion/"datafy.fish").write output
+    (zsh_completion/"datafy").write output
   end
 end
