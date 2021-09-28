5bce87375c853655d78fe158b12d05c939f3f86a
Stijn De Haes <stijndehaes@gmail.com>
2021-09-28 14:31:28 +0200

    upgrade to v0.53.3

--- Formula/datafy.rb
+++ Formula/datafy.rb
@@ -2,14 +2,14 @@ class Datafy < Formula
   desc "Datafy command line interface"
   homepage "https://get.datafy.cloud/"
   on_macos do
-    url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.53.2/datafy_darwin_amd64.tar.gz"
-    sha256 "8aa9e61ab6c4f3afb29ef2131698dda9db39282ea30e498bf221842c98dda74b"
+    url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.53.3/datafy_darwin_amd64.tar.gz"
+    sha256 "8476d4b481b500a7e11f53ef60c7b81bff612823e826b70c11f5bd97f60048ab"
   end
   on_linux do
-    url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.53.2/datafy_linux_amd64.tar.gz"
-    sha256 "a494d7073d5d0fd82cc5a247578f3d6b8d665a4138e50124962fb0ef7fd592fd"
+    url "https://datafy-cp-artifacts.s3-eu-west-1.amazonaws.com/cli/0.53.3/datafy_linux_amd64.tar.gz"
+    sha256 "a1c5bf51443940b2f9e7301e420458db6bb9d880bdd4110a7ed8d4d9622b7f20"
   end
-  version "0.53.2"
+  version "0.53.3"
 
   def install
     bin.install Dir.glob("**/datafy")
